7f0c5f7150d50ed40bfbb90226d0b2b5db7c53a4
pvslvch <polina.vasilevichh@gmail.com>
2025-09-10 17:05:33 +0200

    feat[UI]: implement Header component

---
+++ src/features/header/index.tsx
@@ -0,0 +1,42 @@
+import { Link, NavLink } from "react-router-dom";
+import { ROUTES } from "@/shared/constants/routes";
+import logoImg from "@/assets/img/logo.svg";
+
+import cls from "./header.module.css";
+
+export const links = [
+  {
+    title: "Verbs List",
+    path: ROUTES.VERBS,
+  },
+
+  {
+    title: "Learn Random Verbs",
+    path: ROUTES.RANDOM_VERBS,
+  },
+];
+
+export function AppHeader() {
+  return (
+    <header className="bg-background border-b border-border/40 shadow-sm py-5 px-4 mb-6">
+      <div className="container mx-auto flex items-center justify-between">
+        <Link to={ROUTES.HOME} className="uppercase">
+          {/* <img src={logoImg} alt="logo" className="w-[70px]" /> */}
+          Mighty verbs
+        </Link>
+
+        <nav className="flex items-center gap-4">
+          {links.map((link) => (
+            <NavLink
+              key={link.title}
+              to={link.path}
+              className={({ isActive }) => (isActive ? cls.active : "")}
+            >
+              {link.title}
+            </NavLink>
+          ))}
+        </nav>
+      </div>
+    </header>
+  );
+}
